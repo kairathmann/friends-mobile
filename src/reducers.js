3fb2d6c70a6ca9d3efc4e0bc97a53118828e555d
andrzej_sz <andrzej.szmajnta.dev@gmail.com>
2019-01-23 13:27:16 +0100

    Add error/loading reducers (#14)
    * Add error/loading reducers
    
    * Add lodash
    
    * Install exact

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
+import errorReducer from './store/errors/reducer'
+import loadingReducer from './store/loading/reducer'
 
 export default combineReducers({
-  fake: (state = {}) => state
+	loading: loadingReducer,
+	errors: errorReducer
 })
---
+++ src/store/utils/selectors.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+
+export const createLoadingSelector = actions => state => {
+	return _(actions).some(action => _.get(state, `loading.${action}`))
+}
+
+export const createErrorMessageSelector = actions => state => {
+	return (
+		_(actions)
+			.map(action => _.get(state, `errors.${action}`))
+			.compact()
+			.first() || ''
+	)
+}
